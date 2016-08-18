2c1faac8339c261ac17355983b56d15a13cdd56c
Xiao Yu <me@xyu.io>
2016-08-18 04:39:10 +0000

    Update plugin headers

--- secure-db-connection.php
+++ secure-db-connection.php
@@ -1,12 +1,12 @@
 <?php
-/*
-Plugin name: Secure DB Connection
-Plugin URI: http://wordpress.org/plugins/secure-db-connection/
-Description: Sets SSL keys and certs for encrypted database connections
-Author: Xiao Yu
-Author URI: http://xyu.io/
-Version: 1.0
-*/
+/**
+ * Plugin name: Secure DB Connection
+ * Plugin URI: http://wordpress.org/plugins/secure-db-connection/
+ * Description: Sets SSL keys and certs for encrypted database connections
+ * Author: Xiao Yu
+ * Author URI: http://xyu.io/
+ * Version: 1.0
+ */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
